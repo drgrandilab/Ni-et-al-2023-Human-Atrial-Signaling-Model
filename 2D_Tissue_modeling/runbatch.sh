130d70c328c4875632e2f3353637c7b73329b8b0
Haibo Ni <hbni@ucdavis.edu>
2024-12-19 14:35:12 -0800

    Add files via upload

--- 2D_Tissue_modeling/runbatch.sh
+++ 2D_Tissue_modeling/runbatch.sh
@@ -1,10 +1,10 @@
 sbatch run_2d.slurm 0.1 CaMKII_db 500
-sbatch run_2d.slurm 0.1 Default 500
-sbatch run_2d.slurm 0.1 CaMKII_inhb 500
-sbatch run_2d.slurm 0 Default 500
-sbatch run_2d.slurm 0 CaMKII_db 500
-sbatch run_2d.slurm 0 CaMKII_inhb 500
+# sbatch run_2d.slurm 0.1 Default 500
+# sbatch run_2d.slurm 0.1 CaMKII_inhb 500
+# sbatch run_2d.slurm 0 Default 500
+# sbatch run_2d.slurm 0 CaMKII_db 500
+# sbatch run_2d.slurm 0 CaMKII_inhb 500
 
-sbatch run_2d_CaMKII_exclusion.slurm 0.1 CaMKII_db 500 No_CaMKII_GapG
-sbatch run_2d_CaMKII_exclusion.slurm 0.1 CaMKII_db 500 No_CaMKII_NaV
-sbatch run_2d_CaMKII_exclusion.slurm 0.1 CaMKII_db 500 No_CaMKII_K1
+sbatch run_2d_CaMKII_exclusion_workstation.slurm 0.1 CaMKII_db 500 No_CaMKII_GapG
+sbatch run_2d_CaMKII_exclusion_workstation.slurm 0.1 CaMKII_db 500 No_CaMKII_NaV
+sbatch run_2d_CaMKII_exclusion_workstation.slurm 0.1 CaMKII_db 500 No_CaMKII_K1
